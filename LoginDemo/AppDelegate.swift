8e053b736e3ece33fbbc6526319acbebc916d6ed
astrye <123456789@qq.com>
2021-09-17 16:41:26 +0800

    fix

---
+++ LoginDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  LoginDemo
+//
+//  Created by apple on 2021/9/16.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
